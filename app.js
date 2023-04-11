74b6ce784c3ff09b11555fe098f5bb6630a2c6ac
Chinwe Okpala <chinweamara34@gmail.com>
2023-04-11 17:53:54 -0400

    updates

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+  
+  app.get('/user/:name/:age/:sex', function(req, res, next) {
+    const user = {
+      name: req.params.name,
+      age: req.params.age,
+      sex: req.params.sex
+    };
+    res.json(user);
+  });
+  
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -38,4 +35,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = process.env.PORT || 4000
+
+app.listen(port)
+
 module.exports = app;
+
+
